4a40e281d5f08e6b8cccdacbb82ac0093b676828
Md Faisal Raza Khan <irkfaisal@gmail.com>
2023-07-25 16:34:40 +0530

    m

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/modernwebsite/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
